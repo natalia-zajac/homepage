3aea1cc0c869ad938e08ad706b4a9b2db3c0131a
Natalia Zajac <natalia.zajac@hotmail.com>
2020-06-30 17:58:03 +0200

    Index rrenamed

--- js/home.script.js
+++ js/home.script.js
@@ -1,57 +1,64 @@
-console.log("Cześć wszystkim zarywającym nocki.");
-
-let button__changeHeader = document.querySelector(".button__changeHeader");
-let container__header = document.querySelector(".container__header");
-
-button__changeHeader.addEventListener("click", () => {
-    container__header.innerText = "Natalia_welcome"
-})
-
-console.log(button__changeHeader);
-
-
-
-let button__removeBackground = document.querySelector(".button__removeBackground");
-let container = document.querySelector(".container");
-
-button__removeBackground.addEventListener("click", () => {
-    container.remove();
-});
-
-console.log(button__removeBackground);
-
-
-
-let button__changeBackground = document.querySelector(".button__changeBackground");
-let body = document.querySelector(".body");
-let nextColorName = document.querySelector(".nextColorName");
-
-button__changeBackground.addEventListener("click", () => {
-    body.classList.toggle("yellowBackground")
-    nextColorName.innerText = body.classList.contains("yellowBackground") ? "zimne" : "ciepłe";
-
-});
-
-
-
-let button__hello = document.querySelector(".button__hello");
-
-button__hello.addEventListener("click", () => {
-    if (button__hello.innerText === "Przywitaj się") {
-        button__hello.innerText = "Dzień dobry!";
+{
+    const welcome = () => {
+        console.log("Cześć wszystkim zarywającym nocki.");
     }
-});
-
-console.log (button__hello);
-
-
-
-
-let aside__button = document.querySelector(".aside__button");
-let aside__header = document.querySelector(".aside__header");
-
-aside__button.addEventListener("click", () => {
-    aside__header.innerText = "Odwiedź stony z listy";
-});
-
-console.log(aside__button);
+    
+    welcome();
+    
+    
+    
+    let button__changeHeader = document.querySelector(".button__changeHeader");
+    let container__header = document.querySelector(".container__header");
+    
+    button__changeHeader.addEventListener("click", () => {
+        container__header.innerText = "Natalia_welcome"
+    })
+    
+    console.log(button__changeHeader);
+    
+    
+    
+    let button__removeBackground = document.querySelector(".button__removeBackground");
+    let container = document.querySelector(".container");
+    
+    button__removeBackground.addEventListener("click", () => {
+        container.remove();
+    });
+    
+    console.log(button__removeBackground);
+    
+    const onChangeBackgroundClick = () => {
+        body.classList.toggle("yellowBackground")
+        nextColorName.innerText = body.classList.contains("yellowBackground") ? "zimne" : "ciepłe";
+    };
+    
+    let button__changeBackground = document.querySelector(".button__changeBackground");
+    let body = document.querySelector(".body");
+    let nextColorName = document.querySelector(".nextColorName");
+    
+    button__changeBackground.addEventListener("click", onChangeBackgroundClick);
+    
+    
+    
+    let button__hello = document.querySelector(".button__hello");
+    
+    button__hello.addEventListener("click", () => {
+        if (button__hello.innerText === "Przywitaj się") {
+            button__hello.innerText = "Dzień dobry!";
+        }
+    });
+    
+    console.log (button__hello);
+    
+    
+    
+    
+    let aside__button = document.querySelector(".aside__button");
+    let aside__header = document.querySelector(".aside__header");
+    
+    aside__button.addEventListener("click", () => {
+        aside__header.innerText = "Odwiedź stony z listy";
+    });
+    
+    console.log(aside__button);
+}
